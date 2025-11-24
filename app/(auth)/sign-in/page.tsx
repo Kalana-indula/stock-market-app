7cc40863ea99afe4c1149dfbaa45db66401ad734
Kalana-indula <73228974+Kalana-indula@users.noreply.github.com>
2025-11-24 12:40:52 +0530

    Create sign-in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,66 @@
+'use client';
+
 import React from 'react'
+import {useForm} from "react-hook-form";
+import InputField from "@/components/forms/InputField";
+import {Button} from "@/components/ui/button";
 
 const SignIn = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<SignInFormData>({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        mode: 'onBlur'
+    });
+
+    const onSubmit = async (data: SignInFormData) => {
+        try {
+            // const result = await signInWithEmail(data);
+            // if(result.success) router.push('/');
+        } catch (e) {
+            // console.error(e);
+            // toast.error('Sign in failed', {
+            //     description: e instanceof Error ? e.message : 'Failed to sign in.'
+            // })
+        }
+    }
+
     return (
-        <div>Page</div>
+        <>
+            <h1 className="form-title">
+                Welcome Back
+            </h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                <InputField
+                    name="email"
+                    label="Email"
+                    placeholder="contact@jsmastery.com"
+                    register={register}
+                    error={errors.email}
+                    validation={{ required: 'Email name is required', pattern: /^\w+@\w+\.\w+$/, message: 'Email address is required' }}
+                />
+
+                <InputField
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                    register={register}
+                    error={errors.password}
+                    validation={{ required: 'Password is required', minLength: 8 }}
+                />
+
+                <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                    {isSubmitting ? 'Signing In' : 'Sign In'}
+                </Button>
+            </form>
+        </>
     )
 }
 export default SignIn;
